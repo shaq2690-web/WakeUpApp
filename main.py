f5973f670762606582e2a72278c4d4c7ae2d6656
shaq2690-web <shaq2690@gmail.com>
2025-09-26 18:57:41 +0330

    second update

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("WakeUpApp: Your alarm app")
+from plyer import notification
+import time
+
+# زمان آلارم
+time.sleep(5)  # 5 ثانیه منتظر بمانید
+
+# نمایش آلارم
+notification.notify(
+    title='WakeUpApp',
+    message='وقت بیدار شدن است!',
+    timeout=10  # مدت زمانی که آلارم نمایش داده می‌شود (ثانیه)
+)
